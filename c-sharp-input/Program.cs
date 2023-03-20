ee7662f5ec0f6c079c29e0c544d0e25f3c22ddf8
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-20 23:23:19 +0100

    get input from user  and show the result

--- c-sharp-input/Program.cs
+++ c-sharp-input/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace c_sharp_input // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci il primo numero");
+            // convertiamo automaticamente i dati inseriti in console in int
+            int primoNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci il secondo numero");
+            int secondoNumero = Convert.ToInt32(Console.ReadLine());
+
+            int risultato = primoNumero + secondoNumero;
+
+            Console.WriteLine($"I numeri inseriti sono {primoNumero} e {secondoNumero}. La somma dei due numeri è {risultato}");
         }
     }
 }
